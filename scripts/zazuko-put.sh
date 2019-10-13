bf1fac7ceaeb338a5fe94d1a413e58b29276e0e8
Adrian Gschwend <ktk@netlabs.org>
2019-10-13 21:57:25 +0200

    Remove unused script

--- scripts/zazuko-put.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-curl -n \
-     -X PUT \
-     -H Content-Type:application/n-triples \
-     -T target/everything.nt \
-     -G http://data.zazuko.com/lindas \
-     --data-urlencode graph=https://linked.opendata.swiss/graph/blv/animalpest
